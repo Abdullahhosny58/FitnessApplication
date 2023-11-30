3009f0d06e09386218d57d4975b3e6ceb4abdc5c
Abdullahhosny58 <115153502+Abdullahhosny58@users.noreply.github.com>
2023-11-30 22:13:12 +0200

    Fitness Application

---
+++ src/shared/types.ts
@@ -0,0 +1,16 @@
+export enum SelectedPage {
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "ourclasses",
+  ContactUs = "contactus",
+}
+export interface BenefitType {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
+export interface ClassType {
+  name: string;
+  description?: string;
+  image: string;
+}
